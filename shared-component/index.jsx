177aef51b5f5862498e1aa9165788b8829a0a731
gitdumi <vlad.dumitrescu@proton.me>
2023-07-21 13:30:40 +0300

    fixes

--- shared-component/index.jsx
+++ shared-component/index.jsx
@@ -1,9 +1,7 @@
 import styles from './shared-component.module.css';
 
-export const sharedComponent = () => {
-
-    <div className={styles.container}>
+export const CharedComponent = () => {
+    return <div className={styles.container}>
         This is my shared component with custom css
     </div>
-
 }
