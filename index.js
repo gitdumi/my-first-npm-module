814ec286d069ce4ee2d76d98e73f519e47c25bd4
gitdumi <vlad.dumitrescu@proton.me>
2023-07-21 13:37:24 +0300

    fix

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
-import { sharedComponent } from "./shared-component";
+import { SharedComponent } from "./shared-component";
 import { styles } from "./shared-component.module.css";
 
 const generateMessage = () => {
   return "Hello World!";
 };
 
-module.exports = { generateMessage, sharedComponent, styles };
+module.exports = { generateMessage, SharedComponent, styles };
